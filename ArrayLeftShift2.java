98846a64116f1db7a5c4ad72b29ec89aa48b4208
Sourabh <sourabhp94503@gmail.com>
2023-01-17 22:16:12 +0530

    12-01-2023

---
+++ ArrayLeftShift2.java
@@ -0,0 +1,27 @@
+//Program to left shift the array
+package myPackage;
+import java.util.Scanner;
+
+public class ArrayLeftShift {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int d = (sc.nextInt()) % n;
+		int a[] = new int[n];
+		int t[] = new int[n];
+		for(int i = 0; i < n; i++)
+			a[i] = t[i] = sc.nextInt();
+		if(d > 0) {
+			for(int i = 0; i < n; i++) {
+				if(i < d)
+					t[n - d + i] = a[i];
+				else 
+					t[i - d] = a[i];
+			}
+		}
+		for(int i = 0; i < n; i++)
+			System.out.print(t[i] + " ");
+	}
+
+}
